50f98e1ecd1e336d45612490038709c3d4c83b07
hasithasandunlakshan <hasofficial2002@gmail.com>
2024-04-02 21:05:43 +0530

    Abstract main Done

---
+++ src/AbstractFractory/AbstarctFactoryMain.java
@@ -0,0 +1,16 @@
+package AbstractFractory;
+
+public class AbstarctFactoryMain
+{
+    public static void main(String args[]){
+        AbstractFactory shapefactory=FractoryProducer.getFactory("SHAPE");
+        Shape shape=shapefactory.getShape("CIrcle");
+        shape.draw();
+
+
+
+        AbstractFactory colorfactory=FractoryProducer.getFactory("COLOR");
+       Color color1=colorfactory.getColor("RED");
+     color1.Fill();
+    }
+}
--- src/AbstractFractory/AbstractFactory.java
+++ src/AbstractFractory/AbstractFactory.java
@@ -4,4 +4,5 @@ public abstract class AbstractFactory {
     abstract  Color getColor(String color);
     abstract Shape getShape(String shape);
 
+
 }
