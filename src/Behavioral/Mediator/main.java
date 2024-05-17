63c4b9510ea201746f86a9fcd41a23295235b035
hasithasandunlakshan <hasiofficial2002@gmail.com>
2024-05-17 14:02:00 +0530

    mediator

--- src/Behavioral/Mediator/main.java
+++ src/Behavioral/Mediator/main.java
@@ -11,3 +11,46 @@ class ChatRoom implements ChatMediator {
         System.out.println(user.getName() + ": " + message);
     }
 }
+
+abstract class User {
+    protected ChatMediator mediator;
+    protected String name;
+
+    public User(ChatMediator mediator, String name) {
+        this.mediator = mediator;
+        this.name = name;
+    }
+
+    public abstract void send(String message);
+
+    public abstract String getName();
+}
+
+class ChatUser extends User {
+
+    public ChatUser(ChatMediator mediator, String name) {
+        super(mediator, name);
+    }
+
+    @Override
+    public void send(String message) {
+        mediator.showMessage(message, this);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        ChatMediator mediator = new ChatRoom();
+
+        User user1 = new ChatUser(mediator, "Alice");
+        User user2 = new ChatUser(mediator, "Bob");
+
+        user1.send("Hi Bob!");
+        user2.send("Hello Alice!");
+    }
+}
